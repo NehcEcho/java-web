c3bcb73c93c0bfbb6688561a52fd3c210c658423
NehcEcho <17008861+nehcecho@user.noreply.gitee.com>
2026-05-31 19:59:08 +0800

    feat: add dependency installation to start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,18 @@
 echo ========================================
 echo   Hotel Room Management System - Start
 echo ========================================
+
 echo.
-echo Starting the system...
+echo [1/3] Installing frontend dependencies...
+cd frontend
+npm install
+cd ..
+
+echo.
+echo [2/3] Installing backend dependencies...
+cd backend
+mvn install -DskipTests
+cd ..
+
 echo.
 pause
